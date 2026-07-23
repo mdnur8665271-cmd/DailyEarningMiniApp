6e1133e1b9df5ca8a32dc2bd0c96d69efb3a4fe2
mdnur8665271-cmd <mdnur8665271@gmail.com>
2026-07-23 11:25:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+let balance = 0;
+let adsWatched = 0;
+let todayEarn = 0;
 
+const maxAds = 20;
+const reward = 2;
+
+const balanceText = document.getElementById("balance");
+const adsCount = document.getElementById("adsCount");
+const todayEarnText = document.getElementById("todayEarn");
+const status = document.getElementById("status");
+
+const watchBtn = document.getElementById("watchAd");
+const claimBtn = document.getElementById("claimReward");
+
+watchBtn.onclick = () => {
+
+    if (adsWatched >= maxAds) {
+        status.innerHTML = "❌ Today's ad limit reached.";
+        return;
+    }
+
+    window.open("https://omg10.com/4/11368110", "_blank");
+
+    let sec = 15;
+
+    claimBtn.disabled = true;
+    status.innerHTML = "⏳ Wait " + sec + " seconds...";
+
+    let timer = setInterval(() => {
+
+        sec--;
+
+        status.innerHTML = "⏳ Wait " + sec + " seconds...";
+
+        if (sec <= 0) {
+
+            clearInterval(timer);
+
+            claimBtn.disabled = false;
+
+            status.innerHTML = "✅ Now click Claim Reward.";
+
+        }
+
+    },1000);
+
+};
+
+claimBtn.onclick = () => {
+
+    balance += reward;
+    todayEarn += reward;
+    adsWatched++;
+
+    balanceText.innerHTML = balance + " ৳";
+    adsCount.innerHTML = adsWatched;
+    todayEarnText.innerHTML = todayEarn;
+
+    claimBtn.disabled = true;
+
+    status.innerHTML = "🎉 Reward Added Successfully!";
+
+};
