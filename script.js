90b525dd4a72d41c32981d87d35f137efdadfd48
mdnur8665271-cmd <mdnur8665271@gmail.com>
2026-07-23 16:49:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://ahaybzcmpcuhjlavnpln.supabase.co";
-const SUPABASE_ANON_KEY = "তোমার sb_publishable key";
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://ahaybzcmpcuhjlavnpln.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_nWHvtntUuZeU4vRfpoOHLQ_l4T0oa6t";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-const SUPABASE_URL = "https://ahaybzcmpcuhjlavnpln.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_nWHvtntUuZeU4vRfpoOHLQ_l4T0oa6t";
+
+// Telegram Mini App
+const tg = window.Telegram.WebApp;
+tg.ready();
+
+const user = tg.initDataUnsafe.user;
+
+if (user) {
+    console.log("Telegram User:", user);
+}
+
 let balance = 0;
 let adsWatched = 0;
 let todayEarn = 0;
@@ -35,43 +39,26 @@ watchBtn.onclick = () => {
 
     window.open("https://omg10.com/4/11368110", "_blank");
 
-    let sec = 15;
+    setTimeout(() => {
+        adsWatched++;
+        todayEarn += reward;
+        balance += reward;
 
-    claimBtn.disabled = true;
-    status.innerHTML = "⏳ Wait " + sec + " seconds...";
+        adsCount.innerHTML = adsWatched;
+        todayEarnText.innerHTML = todayEarn;
+        balanceText.innerHTML = balance + " ৳";
 
-    let timer = setInterval(() => {
+        status.innerHTML = "✅ Reward Added";
 
-        sec--;
-
-        status.innerHTML = "⏳ Wait " + sec + " seconds...";
-
-        if (sec <= 0) {
-
-            clearInterval(timer);
-
-            claimBtn.disabled = false;
-
-            status.innerHTML = "✅ Now click Claim Reward.";
-
+        if (adsWatched >= maxAds) {
+            watchBtn.disabled = true;
         }
 
-    },1000);
+        claimBtn.disabled = false;
 
+    }, 15000);
 };
 
 claimBtn.onclick = () => {
-
-    balance += reward;
-    todayEarn += reward;
-    adsWatched++;
-
-    balanceText.innerHTML = balance + " ৳";
-    adsCount.innerHTML = adsWatched;
-    todayEarnText.innerHTML = todayEarn;
-
-    claimBtn.disabled = true;
-
-    status.innerHTML = "🎉 Reward Added Successfully!";
-
+    status.innerHTML = "🎉 Reward Claimed!";
 };
